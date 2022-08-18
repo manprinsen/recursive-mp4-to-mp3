e84d916f642403f492136798d0befc3402de66f2
Manprinsen <tobias.carlbom@gmail.com>
2022-08-18 14:29:21 +0200

    Create mp4-to-mp3.py

---
+++ mp4-to-mp3.py
@@ -0,0 +1,56 @@
+from moviepy.editor import *
+import os
+from threading import Thread
+import time
+
+#directory = "/volume1/music"
+directory = "/volume1/music"
+
+changed = 0
+
+thread_list = []
+delete_list = []
+current_jobs = 0
+CONCURRENT_JOBS = 10
+
+def mp4_to_mp3(mp4, mp3):
+	global current_jobs
+	current_jobs += 1
+	mp4_without_frames = AudioFileClip(mp4)
+	mp4_without_frames.write_audiofile(mp3)
+	mp4_without_frames.close()
+	current_jobs -= 1
+
+#mp4_to_mp3("my_mp4_path.mp4", "audio.mp3")
+
+for path, current_directory, files in os.walk(directory):
+	for file in files:
+		mp4 = os.path.join(path, file)
+		pre, ext = os.path.splitext(mp4)
+		
+		if ext == ".m4a":
+			os.rename(mp4, pre + ".mp4")
+			mp4 = pre + ".mp4"
+			pre, ext = os.path.splitext(mp4)
+		
+		if ext == ".mp4":
+			while current_jobs >= CONCURRENT_JOBS:
+				#print("to many jobs. please wait")
+				time.sleep(1)
+				
+			mp3 = pre + ".mp3"
+			#mp4_to_mp3(mp4, mp3)
+			t = Thread(target=mp4_to_mp3,args=(mp4,mp3))
+			thread_list.append(t)
+			t.start()
+			delete_list.append(mp4)
+			#os.remove(mp4)
+			changed += 1
+
+for t in thread_list:
+	t.join()
+print(f"all {changed} files converted to mp3. starting to delete original files.")
+
+for item in delete_list:
+	os.remove(item)
+print("original files deleted.")
